23fbcb43b29e3185446f259386eae2fd261ef4b9
fumisan <fumitaka.murakami@levelenter.com>
2021-06-24 15:32:07 +0900

    frist commit

---
+++ dist/sample/googlemaps/index.js
@@ -0,0 +1,26 @@
+let map;
+
+function initMap() {
+  function geoSetup() {
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: latitude, lng: longitude },
+        zoom: 100,
+      });
+    }
+
+    function error() {
+      alert("Unable to retrieve your location");
+    }
+
+    if (!navigator.geolocation) {
+      alert("Geolocation is not supported by your browser");
+    } else {
+      alert("Locating…");
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }
+  geoSetup();
+}
